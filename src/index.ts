c90c1680117f397698849da21d7c847d66c3f7f0
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-13 21:55:46 +0700

    Retry Discord.js v13, add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-import { Client } from 'discord.js';
-const client = new Client();
+import { Client, Intents, Role } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    // Intents.FLAGS.GUILD_MEMBERS,
+    // Intents.FLAGS.GUILD_BANS,
+    // Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    // Intents.FLAGS.GUILD_INTEGRATIONS,
+    // Intents.FLAGS.GUILD_WEBHOOKS,
+    // Intents.FLAGS.GUILD_INVITES,
+    // Intents.FLAGS.GUILD_VOICE_STATES,
+    // Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+});
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
 
 client.on('ready', () => {
   console.log(`${client.user!.tag}!`);
 });
 
-client.on('message', async message => {
+client.on('messageCreate', async message => {
   console.log(message.content);
 
   if (message.content.startsWith('!hello')) {
@@ -21,19 +46,19 @@ client.on('message', async message => {
     msg.includes('knife')
   ) {
     // Delete message
-    await message.delete();
     console.log('Muted role added to', message.author.username);
     let mutedRole = message.guild!.roles.cache.find(
-      role => role.name === 'Muted'
+      (role: Role) => role.name === 'Muted'
     );
     let defaultRole = message.guild!.roles.cache.find(
-      role => role.name === 'Default'
+      (role: Role) => role.name === 'Default'
     );
 
     message!.member!.roles.add(mutedRole!);
     message!.member!.roles.remove(defaultRole!);
 
     message.reply('You are muted for 10s!');
+    await message.delete();
 
     setTimeout(() => {
       message.member!.roles.remove(mutedRole!);
@@ -43,4 +68,5 @@ client.on('message', async message => {
 });
 
 const token = process.env.DISCORD_TOKEN;
+
 client.login(token);
